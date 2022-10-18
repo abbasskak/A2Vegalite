40844df4e5ca3339128b8ba51f9f4bddc5bbfe95
abbasskak <63091984+abbasskak@users.noreply.github.com>
2022-10-18 22:26:44 +1100

    Update A2.js

--- A2.js
+++ A2.js
@@ -1,9 +1,19 @@
 var fg_1 = "Colorgraph.json";
 vegaEmbed("#FIRSTGRAPH", fg_1).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
-var fg_2 = "Sizegraph.json";
-vegaEmbed("#SECONDGRAPH", fg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var fg_2 = "Sizegraph.json";
+vegaEmbed("#SECONDGRAPH", fg_2).then(function(result) {
+}).catch(console.error);
+
+var fg_3 = "ColorgraphGDPperCapita.json";
+vegaEmbed("#THIRDGRAPH", fg_3).then(function(result) {
+}).catch(console.error);
+
+var fg_4 = "Barchart.json";
+vegaEmbed("#FOURTHGRAPH", fg_4).then(function(result) {
+}).catch(console.error);
+
+var fg_5 = "Sizegraph2.json";
+vegaEmbed("#FIFTHGRAPH", fg_5).then(function(result) {
+}).catch(console.error);
